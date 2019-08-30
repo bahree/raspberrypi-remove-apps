2a5616f847f8a45fa13ad448a7bcfc49a1e03fd1
Amit Bahree <amitdrop@desigeek.com>
2019-08-30 12:41:11 -0700

    Update remove-apps.sh

--- remove-apps.sh
+++ remove-apps.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 #title           :remove-apps.sh
 #description     :This script will remove apps from Raspian Desktop
-#author		 	 :yuvarajoo
-#date            :201804
-#version         :0.1   
-#website	     :http://www.techworked.com
+#author		 	     :bahree
+#version         :1.0 
+#website	       :http://www.desigeek.com
+#forked from https://github.com/techworked/raspberrypi-remove-apps
 #==============================================================================
 
 
 # remove applications
-sudo apt-get --yes remove --purge minecraft-pi 
+sudo apt-get --yes remove --purge minecraft-pi
 sudo apt-get --yes remove --purge scratch
 sudo apt-get --yes remove --purge wolfram-engine
 sudo apt-get --yes remove --purge debian-reference-*
 sudo apt-get --yes remove --purge epiphany-browser*
-sudo apt-get --yes remove --purge sonic-pi 
+sudo apt-get --yes remove --purge sonic-pi
 sudo apt-get --yes remove --purge supercollider*
 sudo apt-get --yes remove --purge greenfoot
 sudo apt-get --yes remove --purge bluej
@@ -26,9 +26,31 @@ sudo apt-get --yes remove --purge libreoffice*
 sudo apt-get --yes remove --purge claws-mail
 sudo apt-get --yes remove --purge python3-thonny
 sudo apt-get --yes remove --purge geany*
-sudo apt-get --yes remove --purge chromium-browser
 sudo apt-get --yes remove --purge realvnc-vnc-viewer
 sudo apt-get --yes remove --purge realvnc-vnc-server
+sudo apt-get --yes remove --purge dillo
+sudo apt-get --yes remove --purge gpicview
+sudo apt-get --yes remove --purge penguinspuzzle
+sudo apt-get --yes remove --purge oracle-java8-jdk
+sudo apt-get --yes remove --purge openjdk-7-jre
+sudo apt-get --yes remove --purge oracle-java7-jdk
+sudo apt-get --yes remove --purge openjdk-8-jre
+sudo apt-get --yes remove --purge squeak
+sudo apt-get --yes remove --purge qjackctl
+sudo apt-get --yes remove --purge epiphany
+sudo apt-get --yes remove --purge greenfoot
+sudo apt-get --yes remove --purge claws-mail-i18n
+sudo apt-get --yes remove --purge *pygame
+sudo apt-get --yes remove --purge mu-editor
+sudo apt-get --yes remove --purge nuscratch 
+sudo apt-get --yes remove --purge smartsim
+sudo apt-get --yes remove --purge idle3 
+sudo apt-get --yes remove --purge python-minecraftpi 
+sudo apt-get --yes remove --purge python3-minecraftpi
+sudo apt-get --yes remove --purge vlc*
+
+#commented out some of the packages I would want to use
+#sudo apt-get --yes remove --purge chromium-browser
 
 #remove unused directory
 rm -r /home/pi/python_games/
